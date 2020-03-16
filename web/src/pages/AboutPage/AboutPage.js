1fbc25655be12a3d6d2b5bfefae6da10125ae710
Yong Joseph Bakos <ybakos@humanoriented.com>
2020-03-15 22:10:50 -0700

    Add links in HomePage and AboutPage.

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -1,9 +1,23 @@
 const AboutPage = () => {
   return (
-    <div>
-      <h1>AboutPage</h1>
-      <p>Find me in ./web/src/pages/AboutPage/AboutPage.js</p>
-    </div>
+    <>
+      <header>
+        <h1>Redwood Blog</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <p>
+          This is a demo of Redwood.
+        </p>
+        <Link to={routes.home()}>Return home</Link>
+      </main>
+    </>
   )
 }
 
