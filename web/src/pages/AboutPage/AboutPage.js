5f772eacc7b02ed9d10c675dc20e1740f259c194
Yong Joseph Bakos <ybakos@humanoriented.com>
2020-03-15 22:22:11 -0700

    Add BlogLayout.

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -1,23 +1,12 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
 const AboutPage = () => {
   return (
-    <>
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <p>
-          This is a demo of Redwood.
-        </p>
-        <Link to={routes.home()}>Return home</Link>
-      </main>
-    </>
+    <BlogLayout>
+      <p>
+        This is a demo of Redwood.
+      </p>
+    </BlogLayout>
   )
 }
 
