f92ef4620ad56cfafaa6ec4aa37c32de5625add1
Yong Joseph Bakos <ybakos@humanoriented.com>
2020-03-20 15:42:19 -0700

    Extract BlogPost into component. Reuse in cells.

---
+++ web/src/components/BlogPost/BlogPost.js
@@ -0,0 +1,17 @@
+import { Link, routes } from '@redwoodjs/router'
+
+const BlogPost = ({ post }) => {
+  return (
+    <article>
+      <header>
+        <h2>
+          <Link to={routes.blogPost({ id: post.id })}>{post.title}</Link>
+        </h2>
+      </header>
+      <p>{post.body}</p>
+      <div>Posted at: {post.createdAt}</div>
+    </article>
+  )
+}
+
+export default BlogPost
