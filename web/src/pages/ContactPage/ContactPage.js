fe3ec5d64fe001f110e3e8514606ec17cd2f33c4
Yong Joseph Bakos <ybakos@humanoriented.com>
2020-03-22 21:05:49 -0700

    ContactPage: Reset form fields after submit.
    Manually invoke useForm and pass it to the Form component.

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -8,6 +8,7 @@ import {
   FormError
 } from '@redwoodjs/web'
 import { useMutation } from '@redwoodjs/web'
+import { useForm } from 'react-hook-form'
 import BlogLayout from 'src/layouts/BlogLayout'
 
 const CREATE_CONTACT = gql`
@@ -20,9 +21,12 @@ const CREATE_CONTACT = gql`
 
 const ContactPage = (props) => {
 
+  const formMethods = useForm({ mode: 'onBlur' })
+
   const [create, {loading, error}] = useMutation(CREATE_CONTACT, {
     onCompleted: () => {
       alert('Thank you for your submission!')
+      formMethods.reset()
     }
   })
 
@@ -33,7 +37,12 @@ const ContactPage = (props) => {
 
   return (
     <BlogLayout>
-      <Form onSubmit={onSubmit} validation={{ mode: 'onBlur' }} error={error}>
+      <Form
+        onSubmit={onSubmit}
+        validation={{ mode: 'onBlur' }}
+        error={error}
+        formMethods={formMethods}
+      >
         <FormError
           error={error}
           wrapperStyle={{ color: 'red', backgroundColor: 'lavenderblush' }}
@@ -66,10 +75,10 @@ const ContactPage = (props) => {
           errorStyle={{ display: 'block', borderColor: 'red' }}
           validation={{
             required: true,
-            // pattern: {
-            //   value: /[^@]+@[^\.]+\..+/,
-            //   message: 'Please enter a valid email address'
-            // }
+            pattern: {
+              value: /[^@]+@[^\.]+\..+/,
+              message: 'Please enter a valid email address'
+            }
           }}
         />
         <FieldError name="email" style={{ color: 'red' }} />
